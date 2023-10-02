5d8766e08c386d2f453530fdbf934a7c1577d091
unknown <milagos31@gmail.com>
2023-10-02 19:17:33 +0800

    part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+        <span>Homes</span>
+      </header>
+      <section class="content"><app-home></app-home></section>
+    </main>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'house-rental';
+  title = 'homes';
 }
